0647f270478c5db0fe470746c39a7a94a880738d
Shail Teotia <shailteotia2003@gmail.com>
2023-10-31 18:00:48 +0530

    daily update

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import '../styles/modern-normalize.css';
 import '../styles/style.css';
+import '../styles/components/preloader.css';
 import '../styles/components/header.css';
 import '../styles/components/login_register.css';
 import '../styles/components/hero.css';
@@ -10,15 +11,24 @@ import '../styles/components/medicine.css';
 import '../styles/components/footer.css';
 import '../styles/utils.css';
 
+import { customCursor } from './utils/custom-cursor';
 import darkMode from './utils/dark-mode'; 
 import { AllLoginJS } from './login';
 import { initializeBackToTopButton } from './utils/backtoTop';
 import popup_components from './utils/pop-component';
 
+customCursor();
 darkMode(); // theme toggling function
 AllLoginJS();
 initializeBackToTopButton(); // Back-to-Top function
 popup_components(); //pop-up function
+
+// preloader logic
+import { handlePreloader } from './utils/preloader.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+    handlePreloader();
+});
 
 //Sign-out
 const signoutButton = document.getElementById("hide-me2");
